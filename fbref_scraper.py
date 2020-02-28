fa9fcdc2b8a0a11a6f915b5084eac2e01fbc6e76
Doug Hagey <douglas.hagey@gmail.com>
2020-02-28 11:41:40 +0100

    Updating to turn script into a callable module. Also renamed position and team columns to something useful for filtering purposes in Tableau.

--- fbref_scraper.py
+++ fbref_scraper.py
@@ -12,45 +12,48 @@ import time
 
 # Use selenium to get the FPL data from the fbref website and put it into a Pandas Dataframe
 def webscraper(league, passing_url,shooting_url):
-    print('Scraping data for',league)
-    options = Options()
-    options.headless = True
-    driver = webdriver.Firefox(options=options)
-    driver.get(passing_url)
-    time.sleep(10)
-    
-    #info = driver.find_element_by_xpath("//*[@id=\"stats_standard\"]/tbody")
-    assists_df = pd.read_html(driver.page_source)[10]
-    
-    driver.get(shooting_url)
-    time.sleep(10)
-    #info = driver.find_element_by_xpath("//*[@id=\"stats_standard\"]/tbody")
-    shooting_df = pd.read_html(driver.page_source)[10]
-    driver.close()
-    
     try:
+        print('Scraping data for',league)
+        options = Options()
+        options.headless = True
+        driver = webdriver.Firefox(options=options)
+        driver.get(passing_url)
+        time.sleep(10)
+        # Have to use header=1 because there are two headers on the passing table
+        # NOT using header=1 will cause the column names to be oddly named and it won't merge properly later
+        passing_df = pd.read_html(driver.page_source, header=1)[10]
+
+        driver.get(shooting_url)
+        time.sleep(10)
+        #shooting_element = driver.find_element_by_xpath("//*[@id=\"stats_shooting\"]/tbody")
+        shooting_df = pd.read_html(driver.page_source)[10]
+        driver.close()
+
         # Rename the df columns to something more useful
-        assists_df.columns =['Rk','Player','Nation','Position','Squad','90s','Assists','xA', 'Assists-xA','KeyPasses', 'Total Completed Passes', 'Total Attempted Passes', 'Total Pass Completion %', 'Short Completed Passes', 'Short Attempted Passes', 'Short Pass Completion %', 'Medium Completed Passes', 'Medium Attempted Passes', 'Medium Pass Completion %', 'Long Completed Passes', 'Long Attempted Passes', 'Long Pass Completion %', 'Passes to Left', 'Passes to Right', 'Free Kick Passes', 'Through Ball Passes', 'Corner Kicks', 'Throw In Passes', 'Passes into final third', 'Passes Into Penalty Box', 'Crosses into Penalty Box', 'Matches']
-        assists_df = assists_df[assists_df.Player != 'Player']
+
+        passing_df = passing_df.rename(columns={'Pos':'Position','Ast':'Assists','A-xA':'Assists-xA','KP':'KeyPasses','Left':'Passes With LF', 'Right':'Passes with RF', 'FK':'Free Kick Passes', 'TB':'Through Ball Passes', 'CK':'Corner Kicks', 'TI':'Throw Ins', '1/3':'Passes into final third', 'PPA':'Passes Into Penalty Box', 'CrsPA':'Crosses into Penalty Box'})
+        passing_df = passing_df[passing_df.Player != 'Player']
         # Get rid of the rows that have NaN as they are bogus and don't relate to players
-        assists_df = assists_df.fillna(0)
-        
-        shooting_df.columns =['Rk','Player','Nation','Position','Squad','90s','Goals','PK', 'PK Attempts','Shots Total', 'Shots on Target', 'FK Shots', 'Shots on Target %', 'Shots per 90', 'SOT per 90', 'Goals per Shot', 'Goals per SOT', 'xG', 'npxG', 'npxG per Shot', 'Goals minus xG', 'np Goals minus xG ', 'Matches']
+        passing_df = passing_df.fillna(0)
+        print(passing_df.head(5))
+
+        shooting_df = shooting_df.rename(columns={'Pos':'Position','Gls':'Goals','Sh':'Shots Total','FK':'FK Shots','SoT':'Shots on Target', 'FK':'FK Shots','SoT%':'Shots on Target %','Sh/90':'Shots per 90','SoT/90':'SOT per 90','G/Sh':'Goals per Shot','G/SoT':'Goals per SOT','npxG/Sh':'npxG per Shot','G-xG':'Goals minus xG','np:G-xG':'np Goals minus xG'})
+        passing_df = passing_df.rename(columns={'Cmp.1':'Short Passes Completed','Att.1':'Short Attempted Passes','Cmp%.1':'Short Passes Completed%','Cmp.2':'Medium Passes Completed','Att.2':'Medium Passes Attempted','Cmp%.2':'Medium Passes Completed%','Cmp.3':'Long Passes Completed','Att.3':'Long Passes Attempted','Cmp%.3':'Long Passes Completed%','Cmp':'Total Passes Completed','Att':'Total Passes Attempted','Cmp%':'Total Passes Completed%'})
         shooting_df = shooting_df[shooting_df.Player != 'Player']
         # Get rid of the rows that have NaN as they are bogus and don't relate to players
         shooting_df = shooting_df.fillna(0)
-        
-        #Cut out the parts we really don't need
-        assists_df = assists_df[['Rk','Player','Position','Squad','90s','Assists','xA', 'Assists-xA','KeyPasses', 'Total Completed Passes', 'Total Attempted Passes', 'Total Pass Completion %', 'Short Completed Passes', 'Short Attempted Passes', 'Short Pass Completion %', 'Medium Completed Passes', 'Medium Attempted Passes', 'Medium Pass Completion %', 'Long Completed Passes', 'Long Attempted Passes', 'Long Pass Completion %', 'Through Ball Passes', 'Corner Kicks', 'Passes into final third', 'Passes Into Penalty Box', 'Crosses into Penalty Box']]
-        shooting_df = shooting_df[['Rk','Goals','PK', 'PK Attempts','Shots Total', 'Shots on Target', 'FK Shots', 'Shots on Target %', 'Shots per 90', 'SOT per 90', 'Goals per Shot', 'Goals per SOT', 'xG', 'npxG', 'npxG per Shot', 'Goals minus xG', 'np Goals minus xG ']]
+        print(shooting_df.head(5))
         
         #Replace Position indentifiers with something more useful
-        assists_df['Position'] = assists_df['Position'].str.slice(0,2)
+        shooting_df['Position'] = shooting_df['Position'].str.slice(0,2)
         position_map = {'DF':'DEF', 'FW':'FWD', 'MF':'MID'}
-        assists_df = assists_df.replace({'Position': position_map})
-        
+        shooting_df = shooting_df.replace({'Position': position_map})
+        passing_df= passing_df.replace({'Position': position_map})
+        #Drop the duplicate columns before merging
+        passing_df.drop(['Player', 'Nation', 'Position','Squad', 'Age', 'Born', '90s','Matches'], axis=1, inplace=True)
         #Merge the dataframes so that we have all the info together
-        EPL_player_df = pd.merge(assists_df, shooting_df, on='Rk')
+        EPL_player_df = pd.merge(shooting_df, passing_df, on='Rk')
+        print(EPL_player_df.head(5))
         
         # Export to .csv so we can use in tableau]
         #assists_df.to_csv(league+'FBRef_Assists.csv', encoding='utf-8', index=False)
@@ -60,10 +63,13 @@ def webscraper(league, passing_url,shooting_url):
     
     except Exception as e:
         print(e)
+        driver.close()
+        time.sleep(5)
 
-EPL = webscraper('EPL', 'https://fbref.com/en/comps/9/passing/Premier-League-Stats', 'https://fbref.com/en/comps/9/shooting/Premier-League-Stats')
-LaLiga = webscraper('LaLiga', 'https://fbref.com/en/comps/12/passing/La-Liga-Stats','https://fbref.com/en/comps/12/shooting/La-Liga-Stats')
+#EPL = webscraper('EPL', 'https://fbref.com/en/comps/9/passing/Premier-League-Stats', 'https://fbref.com/en/comps/9/shooting/Premier-League-Stats')
+#LaLiga = webscraper('LaLiga', 'https://fbref.com/en/comps/12/passing/La-Liga-Stats','https://fbref.com/en/comps/12/shooting/La-Liga-Stats')
 Bundesliga = webscraper('BundesLiga', 'https://fbref.com/en/comps/20/passing/Bundesliga-Stats', 'https://fbref.com/en/comps/20/shooting/Bundesliga-Stats')
-SerieA = webscraper('SerieA', 'https://fbref.com/en/comps/11/passing/Serie-A-Stats', 'https://fbref.com/en/comps/11/shooting/Serie-A-Stats')
-Ligue1 = webscraper('Ligue1', 'https://fbref.com/en/comps/13/passing/Ligue-1-Stats', 'https://fbref.com/en/comps/13/shooting/Ligue-1-Stats')
+#SerieA = webscraper('SerieA', 'https://fbref.com/en/comps/11/passing/Serie-A-Stats', 'https://fbref.com/en/comps/11/shooting/Serie-A-Stats')
+#Ligue1 = webscraper('Ligue1', 'https://fbref.com/en/comps/13/passing/Ligue-1-Stats', 'https://fbref.com/en/comps/13/shooting/Ligue-1-Stats')
 ChampionsLeague = webscraper('ChampionsLeague', 'https://fbref.com/en/comps/8/passing/Champions-League-Stats', 'https://fbref.com/en/comps/8/shooting/Champions-League-Stats')
+
